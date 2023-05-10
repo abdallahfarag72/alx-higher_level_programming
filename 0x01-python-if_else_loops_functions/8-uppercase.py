cfd63879a9592ba2177c8d94b63c94ba0eae64cf
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-05-10 04:57:20 -0700

    function fizzbuzz

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     for letter in str:
         isLowercase = ord(letter) >= 97 and ord(letter) <= 122
         print("{}".format(chr(ord(letter) - (32 if isLowercase else 0))), end='')
-    print("\n")
+    print("\n", end="")
