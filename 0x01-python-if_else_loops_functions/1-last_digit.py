a4752ab3fcc80680518edb9094971bb1736900f7
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-05-09 19:44:07 -0700

    print alphabet

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+last_digit = (abs(number) % 10) * (-1 if number < 0 else 1)
 last_string = ''
 if last_digit > 5:
     last_string = "and is greater than 5"
