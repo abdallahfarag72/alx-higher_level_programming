dc6e1a4a9bc2d85ff2ebb5c9f15f0649080df7b9
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-05-18 03:02:15 -0700

    add_tuple & multiple_returns

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char == 'C' or char == "C":
+        if char == 'c' or char == 'C':
             continue
         new_string += char
     return new_string
