aa1f0593be39c0d75e1ab6c35406707775f2985f
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-05-30 09:52:03 -0700

    square size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" Defines a Square class """
+
+
+class Square:
+    """
+    This is a class that represents a square.
+
+    Attributes:
+        __size (num): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
