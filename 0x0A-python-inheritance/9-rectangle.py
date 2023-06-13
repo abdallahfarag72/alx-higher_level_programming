84db36dcf3962313907de0bfcf2004ccf9919bb5
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-06-13 14:52:58 -0700

    python inheritance rectangle_area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""
+Defines a class
+"""
+
+
+class BaseGeometry:
+    """A base geometry class."""
+
+    def area(self):
+        """Raises an exception indicating that
+        the area() method is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that the given value is
+        an integer greater than 0."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class that inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Computes the area of the rectangle
+        Returns:
+            The area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle instance
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
