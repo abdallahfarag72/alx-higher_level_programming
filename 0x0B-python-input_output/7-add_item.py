b61685efdc5811126aab5182e3e4b78999a929e1
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-06-17 16:36:14 -0700

    pascal triangle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,9 +7,9 @@ Script that adds all arguments to a Python list and saves them to a file
 
 import sys
 from os import path
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__(
-    '6-load_from_json_file.py').load_from_json_file
+    '6-load_from_json_file').load_from_json_file
 
 args = sys.argv[1:]
 
