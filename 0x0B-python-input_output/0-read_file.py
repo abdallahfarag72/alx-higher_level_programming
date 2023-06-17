fe6e96f809c9cf4047c75ccf14608c6fc1114e9c
Abdallah Azzam <abdallahfarag72@gmail.com>
2023-06-17 15:49:49 -0700

    python i/o read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Defines a function
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+# 0x0B-python-input_output
+All python files are interpreted/compiled on Ubuntu 20.04 LTS using python3 (version 3.8.5)
