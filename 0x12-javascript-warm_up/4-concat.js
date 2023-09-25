a36a21137da7104f9888b266da3dcb3f3cbec00f
Abdallah Azzam <90146895+abdallahfarag72@users.noreply.github.com>
2023-09-25 17:58:20 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-const { argv } = process;
-
-console.log(`${argv[2]} is ${argv[3]}`);
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const { argv } = process;
-
-const convNum = parseInt(argv[2]);
-if (isNaN(convNum)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${convNum}`);
-}
